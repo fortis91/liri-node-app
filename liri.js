0ec4a8ded9e5699994a048370349bf5b3db2fe47
Nigel <neastern101@yahoo.com>
2019-05-08 10:56:14 -0500

    repo setup

---
+++ liri.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+
+//Commnads
+//concert-this
+//spotify-this-song
+//movie-this
+//do-what-it-says
+
